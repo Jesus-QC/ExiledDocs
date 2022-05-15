ac1d7ad452f2639853862636a7b4f631de188668
Jesús <69375249+Jesus-QC@users.noreply.github.com>
2022-05-15 14:06:20 +0200

    updoot

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -2,35 +2,18 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-
+//
+//
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}>
 
-      <div className="title-box">
-          <h1 className="title">
-              EXILED
-          </h1>
-          <h2>
-              <span>EX</span>tended <span>I</span>n-runtime <span>L</span>ibrary for <span>E</span>xternal <span>D</span>evelopment
-          </h2>
-      </div>
-
-        <div className="button-box">
-            <a className="button button--secondary button--lg" to="/">
-                Documentation
-            </a>
-            <a className="button button--secondary button--lg" to="/">
-                Documentation
-            </a>
-            <a className="button button--secondary button--lg" to="/">
-                Documentation
-            </a>
-            <a className="button button--secondary button--lg" to="/">
-                Documentation
-            </a>
+        <div className="main">
+            <b className="title">Exiled</b>
+            <p className="desc">Low-Level plugin framework for SCP: Secret Laboratory servers.</p>
+            <img alt="exiled load" className="vid" src="https://imgur.com/wzxq6AF.gif" />
         </div>
 
     </Layout>
